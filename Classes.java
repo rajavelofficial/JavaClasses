bb4840bea1eafc8d543df16a8ab3458da1a6f732
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-28 22:30:26 +0530

    Creating multible objects in a class

--- Classes.java
+++ Classes.java
@@ -1,7 +1,11 @@
 public class Classes {
     int x = 10;
     public static void main(String[] args) {
-        Classes value = new Classes();
-        System.out.println(value.x);
+        Classes value1 = new Classes();
+        Classes value2 = new Classes();
+
+        System.out.println(value1.x);
+        System.out.println(value2.x);
+
     }
 }
