8e01d590959f893fcfb180325efdece84ac79a9f
rajavelofficial <rajavelofficial2003@gmail.com>
2025-07-02 21:57:13 +0530

    Pubg based Class and Object was created and Successfull Run

---
+++ PubgLogin.java
@@ -0,0 +1,8 @@
+class PubgLogin {
+    public static void main(String[] args) {
+        Pubg map = new Pubg();
+        map.erangal();
+        map.miramar(6000);
+    }
+    
+}
---
+++ pubg.java
@@ -0,0 +1,8 @@
+public class Pubg {
+    static void erangal(){
+        System.err.println("Erangal was one of Famaus Map in Pubg and it's also default map in the game");
+    }
+    public void miramar(int meeter){
+        System.out.println("The Miramar Map is the Biggest map in PUBG approximatlly " + meeter + "m is there.");
+    }
+}
